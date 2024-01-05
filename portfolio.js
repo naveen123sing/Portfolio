98741a4c16e99ea106d8e18b2062f6167684728a
naveen123sing <ns7921963@gmail.com>
2024-01-05 16:38:44 +0530

    second commit

---
+++ portfolio.js
@@ -0,0 +1,6 @@
+const toggle = document.getElementById("toggle")
+const nav = document.getElementById("nav")
+
+toggle.addEventListener("click", ()=>{
+    navlist.classList.toggle("navlist-active")
+})
